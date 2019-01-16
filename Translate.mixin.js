da4d1dfc4017854385ea56c1f659dd9e77b86561
joakimia <joakimamundsen91@gmail.com>
2019-01-16 03:59:58 +0000

    e6d83dc add update of sharing in model -> interpretation if manage sharing link is clicked during edit of existing interpretation

---
+++ Translate.mixin.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+var Translate = {
+    contextTypes: {
+        d2: PropTypes.object.isRequired
+    },
+
+    getTranslation: function getTranslation(key) {
+        return this.context.d2.i18n.getTranslation(key);
+    }
+};
+
+export default Translate;
