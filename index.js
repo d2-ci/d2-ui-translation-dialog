6a4ad771af0349dd76d6120e4f8b65eceb25ffa3
Philip-Larsen-Donnelly <phil@dhis2.org>
2019-08-13 10:57:18 +0000

    48e7018 chore: transifex configuration

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _TranslationDialog = require('./TranslationDialog.component');
+
+var _TranslationDialog2 = _interopRequireDefault(_TranslationDialog);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _TranslationDialog2.default;
